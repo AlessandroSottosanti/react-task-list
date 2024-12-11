345d86a329821dc85ed66e0c7c1b169bea96012c
AlessandroSottosanti <asottosanti2@gmail.com>
2024-12-11 17:34:51 +0100

    feat: count tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,60 @@
 import taskslist from './assets/tasks.js';
 
 function App() {
-const tasks = taskslist.tasks;
+  const tasks = taskslist.tasks;
 
   function getUndoneTasks() {
-    return tasks.map(task => {
-      if(task.state === "backlog" || task.state === "in_progress"){
-         return (<li key={task.id}>task: {task.title} - Priorità: {task.priority} - tempo necessario: {task.estimatedTime} min - Stato: {task.state}</li>);
-      }
-    });
+    const undoneTasks = tasks.filter(task => task.state === "backlog" || task.state === "in_progress");
+    return {
+      count: undoneTasks.length,
+      elements: undoneTasks.map(task => (
+        <li key={task.id}>
+          <ul>
+            <li><strong>{task.title}</strong></li>
+            <li>Priority: {task.priority}</li>
+            <li>Est. time: {task.estimatedTime}</li>
+            <li>State: {task.state}</li>
+          </ul>
+        </li>
+      ))
+    };
   }
 
   function getDoneTasks() {
-    return tasks.map(task => {
-      if(task.state === "completed"){
-        return (<li key={task.id}>task: {task.title} - Priorità: {task.priority} - tempo necessario: {task.estimatedTime} min - Stato: {task.state}</li>);
-      }
-    })
+    const doneTasks = tasks.filter(task => task.state === "completed");
+    return {
+      count: doneTasks.length,
+      elements: doneTasks.map(task => (
+        <li key={task.id}>
+          <ul>
+            <li><strong>{task.title}</strong></li>
+            <li>Priority: {task.priority}</li>
+            <li>Est. time: {task.estimatedTime}</li>
+            <li>State: {task.state}</li>
+          </ul>
+        </li>
+      ))
+    };
   }
+
+  const undoneTasksData = getUndoneTasks();
+  const doneTasksData = getDoneTasks();
 
   return (
     <>
-      <h1>Hej Ikea!</h1>
-      <ul>{getUndoneTasks()}</ul>  
-      <ul>{getDoneTasks()}</ul>
+      <h1 className='title'>Task Manager</h1>
+      <div className='container'>
+        <h2>Current Tasks ({undoneTasksData.count})</h2>
+        <ul>{undoneTasksData.elements}</ul>
+      </div>
+
+      <hr />
+      <div className='container'>
+        <h2>Completed Tasks ({doneTasksData.count})</h2>
+        <ul>{doneTasksData.elements}</ul>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
